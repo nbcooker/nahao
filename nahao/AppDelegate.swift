57323754dff3d78eee6a4f1a55761a238463b317
382148374@qq.com <382148374@qq.com>
2015-06-24 17:18:01 +0800

    swift connect website use http

--- nahao/AppDelegate.swift
+++ nahao/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  nahao
-//
-//  Created by 冰 刘 on 15/6/24.
-//  Copyright (c) 2015年 冰 刘. All rights reserved.
-//
 
 import UIKit
 
--- nahao/ViewController.swift
+++ nahao/ViewController.swift
@@ -1,18 +1,18 @@
-//
-//  ViewController.swift
-//  nahao
-//
-//  Created by 冰 刘 on 15/6/24.
-//  Copyright (c) 2015年 冰 刘. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,HttpProtocol {
 
+    var eHttp:HttpController = HttpController()
+    
+    var tableData:NSArray = NSArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        eHttp.delegate=self
+        eHttp.onSearch("http://douban.fm/j/mine/playlist?channel=0")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +20,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func didRecieveResults(results:NSDictionary) {
+        //println(results)
+      
+        if (results["song"] != nil) {
+            self.tableData = results["song"] as! NSArray
+        }
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+
+        
+        let url = "http://douban.fm/j/mine/playlist?channel=0";
+        let imgURL:NSURL=NSURL(string:url)!
+        let request:NSURLRequest=NSURLRequest(URL: imgURL)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response:NSURLResponse!,data:NSData!,error:NSError!)->Void in
+
+            var jsonResult:NSDictionary=NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
+//            println(jsonResult)
+            
+            if (jsonResult["song"] != nil) {
+                self.tableData = jsonResult["song"] as! NSArray
+                        let firDict:NSDictionary = self.tableData[0] as! NSDictionary
+                        let aid:String = firDict["aid"] as! String
+                        if(!aid.isEmpty) {
+                            let alertView = UIAlertView(title: "alertview", message: "say hello", delegate: nil, cancelButtonTitle: "Cancle")
+                            alertView.show()
+                        } else{
+                            println("444")
+                        }
+                
+                
+            }
+
+        })
+        
+
+    
+        
+    }
 }
 
